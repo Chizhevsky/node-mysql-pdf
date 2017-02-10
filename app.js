9211eebdbadc916e3de1087ff74f36691fa734ba
Chizhevsky <alexvinsent96@yandex.ru>
2017-02-10 12:42:33 +0300

    create connection with mysql db and solve the problem with pdf

--- app.js
+++ app.js
@@ -1,34 +1,72 @@
 var express = require('express'),
   mysql = require('mysql'),
-  myConnection = require('express-myconnection');
+  myConnection = require('express-myconnection'),
+  fs = require('fs'),
+  PDFDocument = require('pdfkit'),
+  blobStream = require('blob-stream'),
+  handlebars = require('express-handlebars')
+  .create({defaultLayout: 'main'});
 
 var app = express();
 
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(require('body-parser').urlencoded({extended: true}));
+
 var dbOptions = {
   host: 'localhost',
-  user: 'user',
-  password: 'root',
-  database: 'test'
+  user: 'root',
+  password: '',
+  database: 'users_db'
 };
 
-app.use(myConnection, (mysql, dbOptions, 'request'));
+app.use(myConnection(mysql, dbOptions, 'request'));
 
 app.get('/', function(req, res) {
-
+  res.render('input');
 });
 
-app.post('/', function(req, res) {
-  var reqObj = req.body;
+app.post('/', function(req, resp) {
+  var searchName = req.body.name;
+  var doc,
+    imgBlob;
   req.getConnection(function(err, conn) {
     if (err) {
-      console.error('sql connection error: ', err);
+      console.err('sql error', err);
     } else {
-      var select = '';//sql-код для поиска имени
-      var query = conn.query(select, /*что-то ещё возможно*/ function(err, result) {
-        if (err) console.error('sql error', err);
-        var namesId = result.namesId;
-        res.json('name_id': namesId);
+      conn.query('SELECT * FROM user WHERE firstName = ?', searchName, function(err, res) {
+        if (err) {
+          console.err('sql error:', err);
+        } else {
+          console.log(res[0]);
+          var writeStream = fs.createWriteStream('file.pdf');
+          doc = new PDFDocument({size: 'LEGAL'});
+          doc.pipe(blobStream());
+          doc.pipe(writeStream);
+          imgBlob = res[0].image;
+          fullName = res[0].firstName + ' ' + res[0].lastName;
+          doc.text(fullName, 100, 100);
+          doc.image(imgBlob, {width: 400});
+          doc.end();
+            conn.query(" UPDATE user SET pdf = 'file.pdf' WHERE firstName = ? ", searchName, function(err, resl) {
+              if (err) {
+                throw err;
+                console.log('sql error:', err);
+                resp.send(JSON.stringify({ result: false }));
+              } else {
+                if (!!resl) {
+                  resp.send(JSON.stringify({ result: true }));
+                }
+              }
+            });
+          fs.unlink('file.pdf');
+        }
       });
     }
-  })
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Listen on port 3000');
 });
